f2cdee7797e91f4a5523a7f0bdc579b2f261162f
iago_oliveira <iagogoliver@gmail.com>
2022-06-07 13:21:30 -0300

    Criado arquivo conexao com banco

---
+++ projBancoSql/BancoProj/Cadastro.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoProj
+{
+    public class Cadastro
+    {
+        Conexao conexao = new Conexao();
+        SqlCommand cmd = new SqlCommand();
+        public string mensagem = "";
+        public Cadastro(string nome, string idade, string cargo, double salario)
+        {
+            //comando sql insert, delete, update
+            cmd.CommandText = "insert into Funcionarios (nome, idade, cargo, salario) values (@nome, @idade, @cargo, @salario)";
+            //parametros
+
+            cmd.Parameters.AddWithValue("@nome",nome);
+            cmd.Parameters.AddWithValue("@idade", idade);
+            cmd.Parameters.AddWithValue("@cargo", cargo);
+            cmd.Parameters.AddWithValue("@salario", salario);
+
+            //conectar com banco
+            try
+            {
+                //Conectar com o banco
+                cmd.Connection = conexao.conectar();
+                //executar comando
+                cmd.ExecuteNonQuery();
+                //desconectar
+                conexao.desconectar();
+                this.mensagem = "Cadastrado com sucesso";
+
+            }
+            catch (SqlException e)
+            {
+                this.mensagem = "Erro ao se conectar com o banco de dados";
+            }
+            
+            //executar comando
+            //desconectar
+            //mostrar mensagem de erro ou sucesso
+
+        }
+    }
+}
---
+++ projBancoSql/BancoProj/Funcionario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoProj
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Cargo { get; set; }
+        public double Salario { get; set; }
+    }
+}

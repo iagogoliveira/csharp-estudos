f34ca8f847189e404242926d8530d1c43e24f09d
iago_oliveira <iagogoliver@gmail.com>
2022-06-06 23:31:03 -0300

    Criado a classe diretor

--- ByteBank/ByteBank/Funcionarios/Funcionario.cs
+++ ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace ByteBank.Funcionarios
 {
-    internal class Funcionario
+    public class Funcionario
     {
+        // 0 - funcionário
+        // 1 - diretor
+        // 2 - designer
+        // 3 - Gerente de conta corrente
+        // 4 - Coordenador
+        // N - ...
+
+        private int _tipo;
+
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+
+
+        public double GetBonificacao()
+        {
+            if(_tipo  == 1)
+            {
+                return Salario;
+            }
+
+
+            return Salario*0.10;
+        }
+
     }
 }

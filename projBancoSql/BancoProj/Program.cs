62864a9255fbdedfd5e51ab59a74519a25a92767
iago_oliveira <iagogoliver@gmail.com>
2022-06-07 18:56:48 -0300

    Função Visualizar Tabela criada

--- projBancoSql/BancoProj/Program.cs
+++ projBancoSql/BancoProj/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace BancoProj
+﻿using System;
+
+namespace BancoProj
 {
     class Program
     {
@@ -16,34 +18,52 @@
 
                 Console.WriteLine("Seja bem vindo ao banco!");
                 Console.WriteLine("Qual operação será efetuada? ");
-                Console.WriteLine("1 - Cadastro 0 - Sair");
+                Console.WriteLine("1 - Cadastro 2 - Deletar 3 - Consultar tabela 0 - Sair");
                 escolha = int.Parse(Console.ReadLine());
 
-                if(escolha == 0)
+                switch (escolha)
                 {
-                    estado = false;
-                }
-                else
-                {
-                    Console.WriteLine("Insira o nome do funcionário: ");
-                    nome = Console.ReadLine();
+                    case 0:
+                        estado = false;
+                        break;
+                    case 1:
+                        Console.WriteLine("Insira o nome do funcionário: ");
+                        nome = Console.ReadLine();
 
-                    Console.WriteLine("Insira a idade do funcionário: ");
-                    idade = Console.ReadLine();
-
-
-                    Console.WriteLine("Insira o cargo do funcionário: ");
-                    cargo = Console.ReadLine();
+                        Console.WriteLine("Insira a idade do funcionário: ");
+                        idade = Console.ReadLine();
 
 
-                    Console.WriteLine("Insira o salario do funcionário: ");
-                    salario = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("Insira o cargo do funcionário: ");
+                        cargo = Console.ReadLine();
 
 
-                    Cadastro novoFuncionario = new Cadastro(nome, idade, cargo, salario);
+                        Console.WriteLine("Insira o salario do funcionário: ");
+                        salario = Double.Parse(Console.ReadLine());
 
-                    Console.WriteLine(novoFuncionario.mensagem);
-                    Console.ReadLine();
+
+                        Cadastro novoFuncionario = new Cadastro(nome, idade, cargo, salario);
+
+                        Console.WriteLine(novoFuncionario.mensagem);
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.WriteLine("Insira o nome do funcionário que será deletado: ");
+                        nome = Console.ReadLine();
+
+                        Deletar deletarFuncionario = new Deletar(nome);
+                        Console.WriteLine(deletarFuncionario.mensagem);
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case 3:
+                        VisualizarTabela tabela = new VisualizarTabela();
+                        Console.WriteLine(tabela.mensagem);
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
                 }
             }
         }
